d7c7b30ffdab3d7b8e1e40c6a3ab2e4baddef46c
Lazanett <125960363+Lazanett@users.noreply.github.com>
2023-07-04 10:20:58 +0200

    Add files via upload

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: lazanett <lazanett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 18:20:26 by lazanett          #+#    #+#             */
-/*   Updated: 2023/06/28 15:39:38 by lazanett         ###   ########.fr       */
+/*   Updated: 2023/07/04 10:07:37 by lazanett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,40 @@
 # include <X11/X.h>
 # include <X11/keysym.h>
 # include <fcntl.h>
+# include <stdbool.h>
 
-//--------------------------------PARSING_MAP.C-----------------------------//
-int	ft_init_tab(char *av);
-int	ft_count_line(int fd);
-char**	ft_malloc_tab(char **tab, int len);
+typedef struct s_struc
+{
+	char	**tab;
+	char	*line;
+	int		fd;
+	int		perso;
+	int		exit;
+	int		collect;
+	int		ligne;
+	int		colonne;
+}	t_struc;
+
+
+//--------------------------------MAIN.C------------------------------------//
+void	ft_init_struc(t_struc *elem);
+int	check_argv(char *av, char *search);
+
 //--------------------------------------------------------------------------//
 
+//--------------------------------PARSING_MAP.C-----------------------------//
+int		ft_init_tab(char *av, t_struc *elem);
+int		ft_map_available(t_struc *elem);
+int		ft_size_map(t_struc *nb);
+int		first_line(t_struc *elem);
+int		ft_count_line(t_struc *elem);
+char	**ft_malloc_tab(t_struc *elem);
+//--------------------------------------------------------------------------//
+
+//--------------------------SIDE_MAP.C---------------------------------------//
+int	first_line(t_struc *nb);
+int	left_col(t_struc *nb);
+int	right_col(t_struc *nb);
+int	last_line(t_struc *nb);
+//--------------------------------------------------------------------------//
 #endif
